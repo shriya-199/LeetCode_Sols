3c6cefe5b15cbdade16bca46cd7165e1865de651
Shriya Verma <150320643+shriya-199@users.noreply.github.com>
2026-03-14 23:37:55 +0530

    Time: 1 ms (99.77%) | Memory: 48.8 MB (68.15%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,50 +1,50 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int x = m+n;
+        int n = nums1.length;
+        int m = nums2.length;
+        int x = n+m;
         int[] arr = new int[x];
-        int i=0;
-        int j=0;
-        int indx=0;
-        while(i<m && j<n)
+        int i = 0;
+        int j = 0;
+        int indx =0;
+        while(i<n && j<m)
         {
             if(nums1[i]<nums2[j])
             {
                 arr[indx++]=nums1[i];
                 i++;
             }
-            else if(nums1[i]==nums2[j])
+            else if(nums1[i]>nums2[j])
+            {
+                arr[indx++]=nums2[j];
+                j++;
+            }
+            else
             {
                 arr[indx++]=nums1[i];
                 arr[indx++]=nums2[j];
                 i++;
                 j++;
             }
-            else
-            {
-                arr[indx++]=nums2[j];
-                j++;
-            }
         }
-        while(i<m)
+        while(i<n)
         {
-            arr[indx++]=nums1[i++];
+            arr[indx++]=nums1[i];
+            i++;
         }
-        while(j<n)
+        while(j<m)
         {
-            arr[indx++]=nums2[j++];
+            arr[indx++]=nums2[j];
+            j++;
         }
-        int low=0;
-        int high=x-1;
+        int low = 0;
+        int high = x-1;
         int mid = (low+high)/2;
-        if(x%2!=0)
+        if(x%2==0)
         {
-            return (double)arr[mid];
+            return (double)(arr[mid]+arr[mid+1])/2.0;
         }
-        else
-        {
-            return (double)(arr[mid]+arr[mid+1])/2;
-        }
+        return (double)(arr[mid]);
+       
     }
 }
