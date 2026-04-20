61cb21f6a9274b12ef7fb374d425fad9cbbf7463
Shriya Verma <150320643+shriya-199@users.noreply.github.com>
2026-04-20 23:50:52 +0530

    Time: 2 ms (43.58%) | Memory: 43.3 MB (45.54%) - LeetSync

---
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDistance(int[] colors) {
+        int ans = 0;
+        for(int i =0;i<colors.length-1;i++)
+        {
+            for(int j =i+1;j<colors.length;j++)
+            {
+                if(colors[i]==colors[j]) continue;
+                else
+                {
+                    ans = Math.max(j-i, ans);
+                }
+            }
+        }
+        return ans;
+    }
+}
