732fb95f1e5673d745ff6cdae4842ed89e95334d
Shriya Verma <150320643+shriya-199@users.noreply.github.com>
2026-07-22 00:40:05 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (19.93%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,25 +10,43 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode res = new ListNode(-1);
-        ListNode temp = res;
-        int carry =0;
-        while(l1!=null || l2!=null || carry!=0)
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+        ListNode dummyNode = new ListNode(-1);
+        ListNode dummyTemp = dummyNode;
+        int carry = 0;
+        while(temp1!=null && temp2!=null)
         {
-            int v1 = (l1!=null)?l1.val:0;
-            int v2 = (l2!=null)?l2.val:0;
-
-            int sum = v1+v2+carry;
-            carry = sum/10;
-
-
-            temp.next = new ListNode(sum%10);
-            temp=temp.next;
-
-            if(l1!=null) l1=l1.next;
-            if(l2!=null) l2=l2.next;
+            int sum = temp1.val+temp2.val;
+            ListNode newNode = new ListNode((sum+carry)%10);
+            dummyTemp.next=newNode;
+            dummyTemp=newNode;
+            carry = (sum+carry)/10;
+            temp1=temp1.next;
+            temp2=temp2.next;
+        }
+        while(temp1!=null)
+        {
+            ListNode newNode = new ListNode((temp1.val+carry)%10);
+            dummyTemp.next=newNode;
+            dummyTemp=newNode;
+            carry = (temp1.val+carry)/10;
+            temp1=temp1.next;
+        }
+        while(temp2!=null)
+        {
+            ListNode newNode = new ListNode((temp2.val+carry)%10);
+            dummyTemp.next=newNode;
+            dummyTemp=newNode;
+            carry = (temp2.val+carry)/10;
+            temp2=temp2.next;
+        }
+        if(carry!=0)
+        {
+            ListNode newNode = new ListNode(1);
+            dummyTemp.next=newNode;
         }
 
-        return res.next;
+        return dummyNode.next;
     }
 }
