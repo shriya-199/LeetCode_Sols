8ea60265ad648b7c0fbe5fd9b2f1117fa77d4bb5
Shriya Verma <150320643+shriya-199@users.noreply.github.com>
2026-07-30 23:17:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (47.13%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int q = word.length()>>3;
+        int r = word.length() & 7;
+        return ((q<<2)+r)*(q+1);
+        
+    }
+}
